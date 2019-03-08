1593db7ee10392d9a96a0acef62f619f3c347cf5
Per Åkerström <akerstrom.perry@gmail.com>
2019-03-08 10:31:28 +0100

    Updated movement

--- Team12_LunarGame/Assets/Scripts/Player/PlayerMovement.cs
+++ Team12_LunarGame/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,45 +1,66 @@
 ﻿using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(CircleCollider2D))]
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _gravity;
     [SerializeField] private float _acceleration;
     [SerializeField] private float _terminalVelocity;
     [SerializeField] private float _rotationSpeed;
+    [SerializeField] private float _bounceForceMultiplier;
     [SerializeField] private LayerMask _collisionLayers;
 
-    private Vector3 _velocity;
-    private BoxCollider2D _collider;
-
+    private Vector2 _velocity;
+    private CircleCollider2D _collider;
+    private const float SkinWidth = 0.03f;
+    
     private void Start()
     {
-        _collider = GetComponent<BoxCollider2D>();
+        _collider = GetComponent<CircleCollider2D>();
     }
 
     private void Update()
+    {        
+        UpdateRotation();
+        UpdateMovement();
+        UpdateTranslation();
+    }
+    private void UpdateRotation()
     {
-        //Gravity
-        _velocity += Vector3.down * _gravity * Time.deltaTime;
-        //Acceleration        
-        float vertical = Input.GetAxisRaw("Vertical");
-        _velocity += transform.up * vertical * _acceleration * Time.deltaTime;
-        _velocity = Vector2.ClampMagnitude(_velocity, _terminalVelocity);
-        //Rotation
         float horizontal = Input.GetAxisRaw("Horizontal");
         transform.Rotate(-transform.forward, horizontal * _rotationSpeed * Time.deltaTime);
-        //Translation
-        UpdateCollision();
-        transform.position += _velocity * Time.deltaTime;
+    }
+    private void UpdateMovement()
+    {
+        _velocity += Vector2.down * _gravity * Time.deltaTime;
+        float vertical = Mathf.Clamp01(Input.GetAxisRaw("Vertical"));
+        _velocity += (Vector2) transform.up * vertical * _acceleration * Time.deltaTime;
+        _velocity = Vector2.ClampMagnitude(_velocity, _terminalVelocity);
+    }
+    private void UpdateTranslation()
+    {
+        Vector2 preHitVelocity = _velocity;
+        RaycastHit2D hit = PhysicsHelper.ApplyNormalForce(Cast, Snap, ref _velocity);
+
+        if (hit.normal != Vector2.zero)
+        {
+            Vector2 bounceDirection = hit.normal;
+            Vector2 bounceVelocity = preHitVelocity - _velocity;
+            float force = Vector2.Dot(hit.normal, bounceVelocity) * -1f;
+            _velocity += bounceDirection.normalized * force * _bounceForceMultiplier;
+        }
+        
+        transform.position += (Vector3) _velocity * Time.deltaTime;
     }
 
-    private void UpdateCollision()
+    private RaycastHit2D Cast()
     {
-        RaycastHit2D hit = Physics2D.BoxCast(transform.position + (Vector3) _collider.offset, _collider.bounds.size, transform.rotation.eulerAngles.z, _velocity.normalized, _velocity.magnitude * Time.deltaTime, _collisionLayers);
+        return Physics2D.CircleCast(transform.position, _collider.radius, _velocity.normalized, _velocity.magnitude * Time.deltaTime + SkinWidth, _collisionLayers);
+    }
+    private void Snap(RaycastHit2D hit)
+    {
         if (hit.normal == Vector2.zero) return;
-        _velocity += (Vector3) hit.normal * -1f * Vector2.Dot(hit.normal, _velocity);
+        transform.position = hit.centroid + hit.normal * SkinWidth;
     }
 
 }
-
-
---
+++ Team12_LunarGame/Assets/Scripts/Utility/PhysicsHelper.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public static class PhysicsHelper
+{
+    public delegate RaycastHit2D RaycastDelegate();
+    public delegate void SnappingDelegate(RaycastHit2D hit);
+    private const int MaxIterations = 50;
+    
+    public static RaycastHit2D ApplyNormalForce(RaycastDelegate rayCastMethod, SnappingDelegate snappingMethod, ref Vector2 velocity)
+    {
+        RaycastHit2D finalHit = new RaycastHit2D();
+        
+        for (int i = 0; i < MaxIterations; i++)
+        {            
+            RaycastHit2D hit = rayCastMethod();
+            if (hit.normal == Vector2.zero) return finalHit;
+
+            finalHit = hit;
+            snappingMethod(hit);
+            velocity += hit.normal * -1f * Vector2.Dot(hit.normal, velocity);
+        }
+        return finalHit;
+    }
+    
+}
