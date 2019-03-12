1e52cb7617970b67584c3bebf5c28794b510e67d
Oscar Olsson <oscar-olsson@live.se>
2019-03-12 11:17:48 +0100

    Added you go into bubble on hit and fly up.

--- Team12_LunarGame/Assets/Scripts/Environment/Bubble_Hazard.cs
+++ Team12_LunarGame/Assets/Scripts/Environment/Bubble_Hazard.cs
@@ -4,14 +4,36 @@ public class Bubble_Hazard : MonoBehaviour
 {
     [Tooltip("Use 1-10 for Speed")]
     public float moveSpeed = 0;
+    public GameObject player;
+    public bool Overlap;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void Start()
     {
-        Debug.Log ("Bubble bobble");
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
-    private void Update()
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Overlap = true;
+            Debug.Log("Bubble bobble");
+            player.GetComponent<PlayerMovement>().enabled = false;
+            player.transform.Translate(0, moveSpeed * Time.deltaTime, 0);
+        }
+    }
+
+    void Update()
     {
         transform.Translate(0, moveSpeed * Time.deltaTime, 0);
+        print(transform.position);
+
+        if (Overlap)
+        {
+            player.transform.position = gameObject.transform.position;
+        }
+
     }
+
+
 }
