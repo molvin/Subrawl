a0d11d01d8609b240174dcefd4de0df639140e5e
Malin <malin.wong@futuregames.nu>
2019-03-13 16:06:11 +0100

    added a tag for player 2, fixed so they (pac man ais) apply damage on inpact to the correct player, as well as de-spawning and timing out, will fix spawning system tomorrow and possibly targeting system (will have to be connected to pickup system tho)

--- Team12_LunarGame/Assets/Scripts/Pickups/PacMan/PacManAIFollow.cs
+++ Team12_LunarGame/Assets/Scripts/Pickups/PacMan/PacManAIFollow.cs
@@ -5,28 +5,38 @@ using UnityEngine;
 
 public class PacManAIFollow : MonoBehaviour{
 
+    public GameObject player;
+    public GameObject player2;
     public float speed;
     private Transform target;
-    //void OnCollisionEnter2D(Collision2D col)
-    //{
-    //    //Check collision name
-    //    Debug.Log("collision name = " + col.gameObject.name);
-    //    if (col.gameObject.name == "Player1")
-    //    {
-    //    gib dmg here to player1??
-    //    Destroy(this.GameObject,0.5f);
-    //    }
-    //    if (col.gameObject.name == "Player2")
-    //    {
-    //    gib dmg here to player2??
-    //        Destroy(this.GameObject,0.5f);
-    //    }
-    //}
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.gameObject.name == "Player 1")
+        {
+            player.GetComponent<PlayerHealth>().TakeDamage();
+            Destroy(this.gameObject, 0.5f);
+        }
+
+        else if (other.gameObject.name == "Player 2")
+        {
+            player2.GetComponent<PlayerHealth>().TakeDamage();
+            Destroy(this.gameObject, 0.5f);
+        }
+
+        // If AI is dodged constantly it will "time out" and die after 15 seconds.
+        Destroy(this.gameObject, 15f);
+
+    }
 
 
 
     void Start()
     {
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+            player2 = GameObject.FindGameObjectWithTag("Player2");
+        }
         //if (Player1PickedUpTheThing == true)
         //{
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>(); 
