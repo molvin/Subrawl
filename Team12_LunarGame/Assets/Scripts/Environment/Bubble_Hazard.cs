d45a2a9db88d872fd81063ccc5bd23620cb5fcf8
Oscar Olsson <oscar-olsson@live.se>
2019-03-13 15:31:31 +0100

    Added a maine menu and fixed some bugs in the bubble bobblee

--- Team12_LunarGame/Assets/Scripts/Environment/Bubble_Hazard.cs
+++ Team12_LunarGame/Assets/Scripts/Environment/Bubble_Hazard.cs
@@ -5,15 +5,19 @@ public class Bubble_Hazard : MonoBehaviour
     [Tooltip("Use 1-10 for Speed")]
     public float moveSpeed = 0;
     public GameObject player;
+    public GameObject player2;
     public bool Overlap;
+    public bool Overlap2;
     public GameObject Coral;
     public float Meter = 0;
     public GameObject parent;
     private float Transparency = .30f;
+    public float Meter2 = 0;
 
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        player2 = GameObject.FindGameObjectWithTag("Player2");
         Coral = GameObject.FindGameObjectWithTag("Coral");
     }
 
@@ -27,6 +31,14 @@ public class Bubble_Hazard : MonoBehaviour
             player.transform.Translate(0, moveSpeed * Time.deltaTime, 0);
         }
 
+        if (other.tag == "Player2")
+        {
+            Overlap2 = true;
+            Debug.Log("Bubble bobble");
+            player2.GetComponent<PlayerMovement>().enabled = false;
+            player2.transform.Translate(0, moveSpeed * Time.deltaTime, 0);
+        }
+
         if (other.tag == "KillZone")
         {
             Destroy(transform.root.gameObject);
@@ -34,7 +46,7 @@ public class Bubble_Hazard : MonoBehaviour
             if (Overlap)
             {
                 Meter = 0;
-                player.GetComponent<PlayerHealth>().TakeDamage();
+                player.GetComponent<PlayerValues>().Die();
                 player.GetComponent<PlayerMovement>().enabled = true;
             }
 
@@ -60,6 +72,12 @@ public class Bubble_Hazard : MonoBehaviour
             player.transform.position = gameObject.transform.position;
         }
 
+
+        if (Overlap2)
+        {
+            player2.transform.position = gameObject.transform.position;
+        }
+
         if (Overlap)
         {
             if (Input.GetButtonDown("Jump"))
@@ -69,11 +87,30 @@ public class Bubble_Hazard : MonoBehaviour
             }
         }
 
+        if (Overlap2)
+        {
+            if (Input.GetButtonDown("Submit"))
+            {
+                Meter2 = Meter2 + 1;
+                GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, Transparency - .1f);
+            }
+        }
+
+        print(Meter2);
+
         if (Meter >= 30)
         {
             Destroy(transform.root.gameObject);
             player.GetComponent<PlayerMovement>().enabled = true;
             Meter = 0;
+            print("You broke free");
+        }
+
+        if (Meter2 >= 30)
+        {
+            Destroy(transform.root.gameObject);
+            player2.GetComponent<PlayerMovement>().enabled = true;
+            Meter2 = 0;
             print("You broke free");
         }
 
