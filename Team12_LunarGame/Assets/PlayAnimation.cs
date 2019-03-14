5f1a4b146474c608b672afc1699f0820a0de5c7d
Oscar Olsson <oscar-olsson@live.se>
2019-03-14 14:47:27 +0100

    Created a new level.

--- Team12_LunarGame/Assets/PlayAnimation.cs
+++ Team12_LunarGame/Assets/PlayAnimation.cs
@@ -4,14 +4,19 @@ public class PlayAnimation : MonoBehaviour
 {
     private Animator anim;
 
-    private void Start()
+    private void Awake()
     {
         anim = GetComponent<Animator>();
     }
-
     public void PlayAnim()
     {
         anim.SetBool("BubblesAreSpawning", true);
+    }
+
+    public void StopAnim()
+    {
+        anim.SetBool("BubblesAreSpawning", false);
+        GetComponent<SpriteRenderer>().enabled = false;
     }
 
     public void Hello()
